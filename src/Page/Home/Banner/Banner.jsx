535ef0cfb01167bf727bfa9939c8c8aa20223ddd
AhaD <abuahad35188@gmail.com>
2024-10-27 13:04:12 +0600

    banner section done

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import './banner.css'
 import appstore from '../../../assets/Link → appstore.png.png';
 import google from '.././../../assets/Link → googleplay.png.jpg'
+ 
 const Banner = () => {
      return (
           <section className="banner">
@@ -20,7 +21,77 @@ const Banner = () => {
                     </div>
                </div>
                <div className="right-side-b">
-                    <p>Lorem, ipsum dolor sit amet con  magni quidem iure, facilis fugit ut recusandae, architecto amet, doloribus ullam maiores modi temporibus repellat.</p>
+                    <ul className='flex justify-between p-3'>
+                         <button className='btn active bg-black border-none outline-none font-[400px] text-[14px] leading-[24px] text-[#fff]'>Tours</button>
+                         <button className='btn bg-white hover:bg-black border-none outline-none font-[400px] text-[14px] leading-[24px] text-[#000000] hover:text-[#fff]'>Hotels</button>
+                         <button className='btn bg-white hover:bg-black border-none outline-none font-[400px] text-[14px] leading-[24px] text-[#000000] hover:text-[#fff]'>Tickets</button>
+                         <button className='btn bg-white hover:bg-black border-none outline-none font-[400px] text-[14px] leading-[24px] text-[#000000] hover:text-[#fff]'>Rental </button>
+                         <button className='btn bg-white hover:bg-black border-none outline-none font-[400px] text-[14px] leading-[24px] text-[#000000] hover:text-[#fff]'>Activities</button>
+                    </ul>
+                    {/*  */}
+                    <div className='w-[464px] h-[193px] border border-red-600 mx-auto rounded-md mt-4 glass'>
+                         <div className="flex justify-around">
+                              <div className="right w-[198px] ms-3">
+                               {/*  */}
+                              <label className="form-control w-full max-w-xs ">
+                                   <div className="label">
+                                        <span className="label-text text-[#000]"> Location</span>   
+                                   </div>
+                                   <select className="select  bg-white text-black outline-none border-none">
+                                        <option disabled selected>New York Usa</option>
+                                        <option>Japan</option>
+                                        <option> India</option>
+                                        <option> Pakistan</option>
+                                   </select>
+  
+                              </label>                               
+                              <label className="form-control w-full max-w-xs mt-1">
+                                   <div className="label">
+                                        <span className="label-text text-[#000]">Checkout</span>   
+                                   </div>
+                                   <select className="select  bg-white text-black outline-none border-none">
+                                        <date   selected>Date</date>
+                                        <option>Star Date</option>
+                                        <option>Harry Potter</option>
+                                        <option>Lord of the Rings</option>
+                                   </select>
+  
+                              </label>                               
+                              </div>
+                              {/*  */}
+                              <div className="left w-[198px]">
+                                    <label className="form-control w-full max-w-xs ">
+                                   <div className="label">
+                                        <span className="label-text text-[#000]">Check In</span>   
+                                   </div>
+                                   <select className="select  bg-white text-black outline-none border-none">
+                                        <option disabled selected>New York Usa</option>
+                                        <option>Japan</option>
+                                        <option> India</option>
+                                        <option> Pakistan</option>
+                                   </select>
+  
+                              </label>                               
+                              <label className="form-control w-full max-w-xs mt-1">
+                                   <div className="label">
+                                        <span className="label-text text-[#000]">Guest</span>   
+                                   </div>
+                                   <select className="select  bg-white text-black outline-none border-none">
+                                        <date   selected>Date</date>
+                                        <option> People</option>
+                                        <option>Harry Potter</option>
+                                        <option>Lord of the Rings</option>
+                                   </select>
+  
+                              </label>
+                              </div>
+                         </div>
+                    </div>
+                    {/* text */}
+                    <div className="flex justify-between items-center gap-11 mx-10 mt-8">
+                          <input type="text" placeholder='Search Here' className='btn w-[130px] bg-white'/> 
+                          <a href="">Need Some Help?</a>
+                    </div>
                </div>
               </div>
           </section>
